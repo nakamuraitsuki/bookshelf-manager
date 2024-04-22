7995c21d27a0405f960c8bfaca6699f4cd5cdffa
nakamuraitsuki <isukin@gmail.com>
2024-04-22 19:55:09 +0900

    作り直し

--- server/routes/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const booksController = require('../controllers/booksController');
-
-const router = express.Router();
-
-// ブックリソースのエンドポイント
-router.get('/books', booksController.getAllBooks);
-
-module.exports = router;
