b6329d27e3bbdb950da8161e2f8a02866d2be5e0
nakamuraitsuki <isukin@gmail.com>
2024-04-30 12:34:22 +0900

    見た目だけ作った

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import style from "./Home.module.css";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className={style.titleContainer}>
+                <h1 className={style.title}>埼玉大学推理小説研究会蔵書管理</h1>
+            </div>
+            <div className={style.linkContainer}>
+                <Link to="/add" className={style.link}>蔵書追加はこちら</Link>
+            </div>
+            <h2 className={style.title}>蔵書検索</h2>
+            
         </div>
     );
 }
