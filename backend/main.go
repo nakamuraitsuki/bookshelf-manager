81b8b2bec5e985ec19a3976e5375ac8c15abdf62
nakamuraitsuki <isukin@gmail.com>
2024-05-02 17:44:45 +0900

    データベース周り

--- backend/main.go
+++ backend/main.go
@@ -7,14 +7,29 @@ import (
 	_"github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	dbPath := "my-database.db"
+const (
+	dbPath = "database.db"
+	createTableQuery = `
+		CREATE TABLE IF NOT EXISTS books (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			author TEXT NOT NULL,
+			publisher TEXT NOT NULL,
+			isbn TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+	`
+)
 
+func init() {
 	db, err :=sql.Open("sqlite3", dbPath)
 	if err != nil {
 		fmt.Println("データベース接続失敗",err)
 		return
 	}
 	defer db.Close()
+
+}
+func main() {
 	fmt.Println("データベースに接続しました")
 }
