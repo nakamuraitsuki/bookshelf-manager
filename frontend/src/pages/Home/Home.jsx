78badbba3e22e75407346a003ec0dd6ecd4d1cd4
nakamuraitsuki <isukin@gmail.com>
2024-05-02 14:19:40 +0900

    react-routerとhome画面まで

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+    return (
+        <div>
+            <div className = "Title">
+                <h1>home</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
