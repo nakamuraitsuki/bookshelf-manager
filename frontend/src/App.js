38df46b9706aa3f787f8027f2281ad2fca0efe77
nakamuraitsuki <isukin@gmail.com>
2024-05-03 10:28:17 +0900

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Add from './pages/Add/Add';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route path = "/add" element={<Add/>}/>
         </Routes>
       </BrowserRouter>
     </div>
