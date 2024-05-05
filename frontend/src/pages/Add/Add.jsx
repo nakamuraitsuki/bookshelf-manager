ef80a546be77cc711bf1bc6a893253c62f281d4e
nakamuraitsuki <isukin@gmail.com>
2024-05-05 19:38:02 +0900

    本の情報を取得するところまで完了

--- frontend/src/pages/Add/Add.jsx
+++ frontend/src/pages/Add/Add.jsx
@@ -2,21 +2,39 @@ import React ,{useState} from "react";
 import styles from "./Add.module.css"
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import {XMLParser, XMLBuilder} from 'fast-xml-parser';
 
 const Add = () => {
     const navigate = useNavigate();
+    const parser = new XMLParser({
+        ignoreAttributes:false,
+        attributeNamePrefix: '',
+    });
+    const builder = new XMLBuilder();
     const [bookInfo,setBookInfo] = useState(null);
     const [error,setError] = useState(null);
-    //const corsProxy = 'https://cors-anywhere.herokuapp.com/';//CORSプロキシ
     function addBook() {
         //入力を取ってくる
         const isbn = document.querySelector('input[name="isbn"]').value;
-        const endpoint = `https://ndlsearch.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRecords=10&query=title%3d%22%E6%A1%9C%22%20AND%20from%3d%222018%22`;
+        //国立国会図書館API
+        const endpoint = `https://ndlsearch.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRecords=1&query=isbn%3D${isbn}`;
 
         axios.get(endpoint)
         .then(response => {
-            setBookInfo(response.data);
-            console.log('ok');
+            //データ構造の都合で2回パース
+            const parsedData = parser.parse(response.data);
+            const recordData = parsedData.searchRetrieveResponse.records.record.recordData;
+            const bookParsedData = parser.parse(recordData);
+            //それぞれを格納
+            const bookTitle = bookParsedData['srw_dc:dc']['dc:title'];
+            const bookCreator = bookParsedData['srw_dc:dc']['dc:creator'];
+            const bookPublisher = bookParsedData['srw_dc:dc']['dc:publisher'];
+            const bookLanguage = bookParsedData['srw_dc:dc']['dc:language'];
+            
+            setBookInfo({ title: bookTitle, creator: bookCreator, publisher: bookPublisher, language: bookLanguage });
+
+            //デバック用
+            console.log('title:', bookInfo.title);
         })
         .catch(error => {
             setError('書籍情報の取得に失敗しました');
@@ -32,7 +50,7 @@ const Add = () => {
             <h1>Add page</h1>
             <div>
                 {/*TODO:10桁又は13桁の数字しか入れられないようにする（空白を許さない） */}
-                <label>ISBN:<input id="isbn" name="isbn" type="number"/></label>                
+                <label>ISBN:<input id="isbn" name="isbn" type="text"/></label>                
             </div>
             <div className={styles.submitButton}>
                 <label>
