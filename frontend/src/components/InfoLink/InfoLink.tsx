8f2c637394b20ac905ba6d22f656f99fa70cfd23
nakamuraitsuki <isukin@gmail.com>
2024-08-11 11:42:37 +0900

    css変更

--- frontend/src/components/InfoLink/InfoLink.tsx
+++ frontend/src/components/InfoLink/InfoLink.tsx
@@ -20,7 +20,7 @@ export const InfoLink: React.FC<Props> = ({book,to}) =>{
     return(
         <div>
             <Link to={`/book/${to}`} className={styles.card}>
-                <span className={styles.title}>{book.title} / {book.author} / {book.publisher}</span>
+                <span className={styles.text}>{book.title} / {book.author} / {book.publisher}</span>
             </Link>
         </div>
     );
