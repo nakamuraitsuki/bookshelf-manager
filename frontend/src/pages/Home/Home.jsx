8218edc56151aaf77c79090969799af5cd7c4f0f
nakamuraitsuki <isukin@gmail.com>
2024-08-12 15:10:47 +0900

    home体裁

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import { FaBookMedical } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+
 const Home = () => {
     return (
         <div>
-            <div className = {styles.Title}>
-                <h1>home</h1>
-            </div>
-            <div>
-                <Link to="/add">蔵書追加はこちら</Link>
-                <Link to="/search/byTitle">検索はこちら</Link>
+            <h1 className={styles.Title}>home</h1>
+            <div className={styles.cardField}>
+                <Link to="/add" className={`${styles.card} ${styles.addBook}`}>
+                    <FaBookMedical className={styles.cardIcon}/>
+                    <span className={styles.cardText}>Add Book</span>
+                </Link>
+                <Link to="/search/byTitle" className={`${styles.card} ${styles.searchBook}`}>
+                    <FaSearch className={styles.cardIcon}/>
+                    <span className={styles.cardText}>Search Book</span>
+                </Link>
             </div>
         </div>
     )
