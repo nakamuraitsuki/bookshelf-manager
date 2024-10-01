7ae6d0907c57dae129087d6cf7e8914f3d3889d3
nakamuraitsuki <isukin@gmail.com>
2024-10-01 17:23:47 +0900

    fix

--- frontend/src/components/NormalButton/NormalButton.tsx
+++ frontend/src/components/NormalButton/NormalButton.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './NormalButton.module.css'
 
 type Props = {
+    type: "submit"|"reset" | "button" | undefined,
     label: string,
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const NormalButton: React.FC<Props> = ({label, onClick}) => {
+export const NormalButton: React.FC<Props> = ({type, label, onClick}) => {
     return(
-        <button type='button' onClick={onClick} className={styles.normalButton}>{label}</button>
+        <button type={type} onClick={onClick} className={styles.normalButton}>{label}</button>
     )
 }
