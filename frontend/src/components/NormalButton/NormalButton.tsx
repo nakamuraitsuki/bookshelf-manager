1803a76410049ee8aa7dabe7b5059ba25daf3089
nakamuraitsuki <isukin@gmail.com>
2024-10-01 12:59:26 +0900

    normalButton追加

---
+++ frontend/src/components/NormalButton/NormalButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './NormalButton.module.css'
+
+type Props = {
+    label: string,
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const NormalButton: React.FC<Props> = ({label, onClick}) => {
+    return(
+        <button onClick={onClick} className={styles.normalButton}>{label}</button>
+    )
+}
